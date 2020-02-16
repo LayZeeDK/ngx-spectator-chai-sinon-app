c515616cadef8a79d24a980a533f6ac602ef95f7
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2020-02-16 01:36:30 +0100

    test: convert to local Chai

--- src/app/button/button.component.spec.ts
+++ src/app/button/button.component.spec.ts
@@ -1,6 +1,9 @@
 import { createComponentFactory, Spectator } from '@ngneat/spectator';
+import * as chai from 'chai';
 
 import { ButtonComponent } from './button.component';
+
+const expect = chai.expect;
 
 describe('ButtonComponent', () => {
   let spectator: Spectator<ButtonComponent>;
@@ -9,12 +12,20 @@ describe('ButtonComponent', () => {
   beforeEach(() => spectator = createComponent());
 
   it('should have a success class by default', () => {
-    expect(spectator.query('button')).toHaveClass('success');
+    // expect(spectator.query('button')).toHaveClass('success');
+    const button = spectator.query('button') as Element;
+    const buttonClasses = Array.from(button.classList);
+    expect(buttonClasses).to.contain('success');
   });
 
   it('should set the class name according to the [className] input', () => {
     spectator.setInput('className', 'danger');
-    expect(spectator.query('button')).toHaveClass('danger');
-    expect(spectator.query('button')).not.toHaveClass('success');
+
+    // expect(spectator.query('button')).toHaveClass('danger');
+    // expect(spectator.query('button')).not.toHaveClass('success');
+    const button = spectator.query('button') as Element;
+    const buttonClasses = Array.from(button.classList);
+    expect(buttonClasses).to.contain('danger');
+    expect(buttonClasses).not.to.contain('success');
   });
 });
